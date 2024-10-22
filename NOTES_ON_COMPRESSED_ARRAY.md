93e6bbe80eb1a4d0829035739e69deaebd78e4df
Joel Watson <63216246+nacioboi@users.noreply.github.com>
2024-10-23 01:42:22 +1000

    Add notes on compressed array implementation

---
+++ NOTES_ON_COMPRESSED_ARRAY.md
@@ -0,0 +1,6 @@
+# Things i learned.
+
+- Using multiplier can work so long as we store the remainder as well. This is because not all values divide evenly by the range of multipliers 1-16.
+
+- Instead it might be easier to just use more bits to store the difference.
+  - And thats what i did. I used 30 bits instead of 16.
--- dam/compressed_array.go
+++ dam/compressed_array.go
@@ -1,5 +1,7 @@
 package dam
 
+import "fmt"
+
 const (
 	////////////////////////////// 0. 123456789
 	/////////////////////////////////////// 1. 0123456789
@@ -8,27 +10,20 @@ const (
 	///////////////////////////////////////////////////////////////////// 4. 0123456789
 	/////////////////////////////////////////////////////////////////////////////// 5. 0123456789
 	///////////////////////////////////////////////////////////////////////////////////////// 6. 01234
-	c__BITMASK__64th_B      uint64 = 0b1000000000000000000000000000000000000000000000000000000000000000
-	c__BITMASK__BUT_64      uint64 = 0b0111111111111111111111111111111111111111111111111111111111111111
-	c__BITMASK__EXTRA_INFOS uint64 = 0b0111111111111111000000000000000000000000000000000000000000000000
-	c__BITMASK__SIGNS_INFOS uint64 = 0b0111000000000000000000000000000000000000000000000000000000000000
-	////////////////////////////////// 432109876543210
-	///////////////////////////////////////////////// 5432109876543210
-	///////////////////////////////////////////////////////////////// 5432109876543210
-	///////////////////////////////////////////////////////////////////////////////// 5432109876543210
-	c__BITMASK__MULTIPLIERS uint64 = 0b0000111111111111000000000000000000000000000000000000000000000000
-	c__BITMASK__OUR_BITS    uint64 = 0xFFFF
+	c__BITMASK__64th_B uint64 = 0b1000000000000000000000000000000000000000000000000000000000000000
+	c__BITMASK__BUT_64 uint64 = 0b0111111111111111111111111111111111111111111111111111111111111111
+	/////////////////////////////////// 123456789012345 // 15 bits metadata.
+	c__BITMASK__EXTRA_INFOS uint64 = 0b0111000000000000000000000000000000000000000000000000000000000000
+	/////////////////////////////////// 123 // a sign bit for each difference value.
+	c__BITMASK__SIGNS_INFOS uint64 = 0b0110000000000000000000000000000000000000000000000000000000000000
+	// Bitmask for a single difference value.
+	c__BITMASK__OUR_BITS uint64 = (1<<30 - 1)
 
 	// The amount of right shift required to bring the extras to the start of the 64-bit integer.
-	c__SHIFT_WIDTH__EXTRAS uint64 = 48
-	// The amount of right shift, on-top of right shift by `c__SHIFT_WIDTH__EXTRAS`,
-	// required to bring the sign infos to the start of the 64-bit integer.
-	c__NUM_BITS_FOR_MULTIPLIERS uint64 = 12
+	c__SHIFT_WIDTH__EXTRAS uint64 = 60
 
-	c__NUM_BITS_PER_VALUE       uint64 = 16
-	c__NUM_BITS_PER_MULTIPLIER  uint64 = 4
-	c__MAX_MULTIPLIER           uint64 = 16 // 1-based instead of zero-based
-	c__MAX_NUM_VALUES_PER_ENTRY uint64 = 3
+	c__NUM_BITS_PER_VALUE       uint64 = 30
+	c__MAX_NUM_VALUES_PER_ENTRY uint64 = 2
 )
 
 // The compressed array stores information in two types of entries:
@@ -37,17 +32,23 @@ const (
 //
 // - Checkpoint: Contains an optimized base value.
 type Compressed_Array struct {
-	D []uint64
+	D           *[]uint64
+	spare       []uint64
+	remappings  map[uint64]uint32
+	safety_flag bool
 }
 
 func New_Compressed_Array() *Compressed_Array {
+	data := make([]uint64, 0)
 	return &Compressed_Array{
-		D: make([]uint64, 0),
+		D:          &data,
+		spare:      make([]uint64, 0),
+		remappings: make(map[uint64]uint32),
 	}
 }
 
 func (ca *Compressed_Array) is_checkpoint(idx uint64) bool {
-	v := ca.D[idx]
+	v := (*ca.D)[idx]
 	return v&c__BITMASK__64th_B == c__BITMASK__64th_B
 }
 
@@ -55,13 +56,13 @@ func (ca *Compressed_Array) get_checkpoint_value(idx uint64) uint64 {
 	if !ca.is_checkpoint(idx) {
 		panic("not a checkpoint")
 	}
-	return ca.D[idx] & c__BITMASK__BUT_64
+	return (*ca.D)[idx] & c__BITMASK__BUT_64
 }
 
 func (ca *Compressed_Array) get_num_attached_values(checkpoint_idx uint64) uint64 {
 	num_attached_values := uint64(0)
 
-	for i := checkpoint_idx + 1; i < uint64(len(ca.D)); i++ {
+	for i := checkpoint_idx + 1; i < uint64(len(*ca.D)); i++ {
 		if ca.is_checkpoint(i) {
 			break
 		}
@@ -74,7 +75,7 @@ func (ca *Compressed_Array) get_num_attached_values(checkpoint_idx uint64) uint6
 func (ca *Compressed_Array) needs_new_checkpoint(checkpoint_idx uint64, value uint64) bool {
 	diff, _, _ := ca.calculate_difference(ca.get_checkpoint_value(checkpoint_idx), value)
 
-	if diff >= (c__BITMASK__OUR_BITS * c__MAX_MULTIPLIER) {
+	if diff >= (c__BITMASK__OUR_BITS) {
 		return true
 	}
 
@@ -92,8 +93,9 @@ func (ca *Compressed_Array) get_previous_checkpoint_idx(starting_idx uint64) uin
 
 func (ca *Compressed_Array) create_checkpoint(recommended_value uint64) {
 	// Create a new checkpoint with the recommended value
+	fmt.Printf("create_checkpoint: %d\n", recommended_value)
 	checkpoint := c__BITMASK__64th_B | recommended_value
-	ca.D = append(ca.D, checkpoint)
+	(*ca.D) = append((*ca.D), checkpoint)
 }
 
 // Calculates value - checkpoint_value, checks if it fits in 16 bits.
@@ -115,125 +117,81 @@ func (ca *Compressed_Array) calculate_difference(checkpoint_value, value uint64)
 		diff = -diff
 		is_negative = true
 	}
-	if diff >= int64(c__BITMASK__OUR_BITS*c__MAX_MULTIPLIER) {
+	if diff >= int64(c__BITMASK__OUR_BITS) {
 		return 0, true, is_negative
 	}
 	return uint64(diff), false, is_negative
 }
 
 func (ca *Compressed_Array) apply_sign_infos(idx uint64, j uint64, is_negative bool) {
-	sign_infos := ca.D[idx] & c__BITMASK__SIGNS_INFOS
-	shifted := sign_infos >> (c__SHIFT_WIDTH__EXTRAS + (c__NUM_BITS_FOR_MULTIPLIERS - 1))
-	m := uint64(1) << (3 - j)
+	sign_infos := (*ca.D)[idx] & c__BITMASK__SIGNS_INFOS
+	shifted := sign_infos >> (c__SHIFT_WIDTH__EXTRAS)
+	m := uint64(1) << (c__MAX_NUM_VALUES_PER_ENTRY - j)
 	if is_negative {
 		shifted |= m
 	} else {
 		shifted &= (^m & 0b1111)
 	}
-	mask := shifted << (c__SHIFT_WIDTH__EXTRAS + (c__NUM_BITS_FOR_MULTIPLIERS - 1))
-	ca.D[idx] |= mask
+	mask := shifted << (c__SHIFT_WIDTH__EXTRAS)
+	(*ca.D)[idx] |= mask
 }
 
 func (ca *Compressed_Array) get_is_negative(idx, j uint64) bool {
-	sign_infos := ca.D[idx] & c__BITMASK__SIGNS_INFOS
-	shifted := sign_infos >> (c__SHIFT_WIDTH__EXTRAS + (c__NUM_BITS_FOR_MULTIPLIERS - 1))
-	m := uint64(1) << (3 - j)
+	sign_infos := (*ca.D)[idx] & c__BITMASK__SIGNS_INFOS
+	shifted := sign_infos >> (c__SHIFT_WIDTH__EXTRAS)
+	m := uint64(1) << (c__MAX_NUM_VALUES_PER_ENTRY - j)
 	return shifted&m != 0
 }
 
-func (ca *Compressed_Array) set_multiplier(idx uint64, j uint64, multiplier uint64) {
-	multiplier_infos := ca.D[idx] & c__BITMASK__MULTIPLIERS
-
-	m1 := (multiplier >> 0) & 0b1
-	m2 := (multiplier >> 1) & 0b1
-	m3 := (multiplier >> 2) & 0b1
-	m4 := (multiplier >> 3) & 0b1
-
-	shifted := multiplier_infos >> (c__SHIFT_WIDTH__EXTRAS)
-	shifted &= 0b111111111111
-
-	base_shifter := c__NUM_BITS_FOR_MULTIPLIERS - ((1 + j) * 4)
-	m1_shifter := base_shifter + 3
-	m2_shifter := base_shifter + 2
-	m3_shifter := base_shifter + 1
-	m4_shifter := base_shifter + 0
-
-	if m1 == 1 { // m1
-		shifted |= m1 << m1_shifter
-	} else {
-		shifted &= ^(m1 << m1_shifter)
-	}
-
-	if m2 == 1 { // m2
-		shifted |= m2 << m2_shifter
-	} else {
-		shifted &= ^(m2 << m2_shifter)
-	}
-
-	if m3 == 1 { // m3
-		shifted |= m3 << m3_shifter
-	} else {
-		shifted &= ^(m3 << m3_shifter)
-	}
-
-	if m4 == 1 { // m4
-		shifted |= m4 << m4_shifter
-	} else {
-		shifted &= ^(m4 << m4_shifter)
-	}
-
-	mask := shifted << (c__SHIFT_WIDTH__EXTRAS)
-	ca.D[idx] |= mask
-}
-
-func (ca *Compressed_Array) get_multiplier(idx uint64, j uint64) uint64 {
-	multiplier_infos := ca.D[idx] & c__BITMASK__MULTIPLIERS
-
-	shifted := multiplier_infos >> (c__SHIFT_WIDTH__EXTRAS)
-	shifted &= 0b111111111111
-
-	base_shifter := c__NUM_BITS_FOR_MULTIPLIERS - ((1 + j) * 4)
-	m1_shifter := base_shifter + 3
-	m2_shifter := base_shifter + 2
-	m3_shifter := base_shifter + 1
-	m4_shifter := base_shifter + 0
-
-	var (
-		x1 uint64
-		x2 uint64
-		x3 uint64
-		x4 uint64
-	)
-
-	x1 = (shifted >> m1_shifter) & 0b1
-	x2 = (shifted >> m2_shifter) & 0b1
-	x3 = (shifted >> m3_shifter) & 0b1
-	x4 = (shifted >> m4_shifter) & 0b1
-
-	x := (x1 << 0) | (x2 << 1) | (x3 << 2) | (x4 << 3)
-
-	return x
-}
-
-func (ca *Compressed_Array) find_appropriate_multiplier(diff uint64) (uint64, uint64) {
-	// Calculate the multiplier
-	appropriate_multiplier := uint64(1)
-	did_find_multiplier := false
-	for appropriate_multiplier < c__MAX_MULTIPLIER {
-		if (diff / appropriate_multiplier) < c__BITMASK__OUR_BITS {
-			if diff%appropriate_multiplier == 0 {
-				did_find_multiplier = true
-				break
-			}
+func (ca *Compressed_Array) search_for_matching_checkpoint(value uint64) (uint64, bool) {
+	fmt.Printf("search_for_matching_checkpoint: %d\n", value)
+	// Search for a checkpoint that fits the value
+	for i := uint64(0); i < uint64(len(*ca.D)); i++ {
+		if !ca.is_checkpoint(i) {
+			continue
 		}
-		appropriate_multiplier++
+		checkpoint_value := ca.get_checkpoint_value(i)
+		_, needs_new_checkpoint, _ := ca.calculate_difference(checkpoint_value, value)
+		if !needs_new_checkpoint {
+			return i, true
+		}
 	}
-	if !did_find_multiplier {
-		panic("could not find appropriate multiplier")
+	return 0, false
+}
+
+func (ca *Compressed_Array) handle_restructuring(matching_checkpoint uint64, value uint64) {
+	fmt.Printf("matching_checkpoint: %d\n", matching_checkpoint)
+	if ca.safety_flag {
+		panic("safety flag is set, recursion is not allowed")
 	}
-	if appropriate_multiplier != 1 {
+
+	// A restructure occurs when we need to insert an item.
+	ca.spare = make([]uint64, 0)
+	the_rest := make([]uint64, 0)
+	i := uint64(0)
+	for j, v := range *ca.D {
+		if j == int(matching_checkpoint) {
+			break
+		}
+		ca.spare = append(ca.spare, v)
+		i++
 	}
-	return appropriate_multiplier, diff / appropriate_multiplier
+	for j := matching_checkpoint + 1; j < uint64(len(*ca.D)); j++ {
+		the_rest = append(the_rest, (*ca.D)[j])
+	}
+
+	// Insert the new value
+	ca.D = &ca.spare
+
+	// Simply use the append function to add the rest of the values.
+	ca.safety_flag = true
+	defer func() { ca.safety_flag = false }()
+
+	ca.Append(value)
+
+	for _, v := range the_rest {
+		ca.Append(v)
+	}
 }
 
 func (ca *Compressed_Array) Append(value uint64) {
@@ -243,18 +201,23 @@ func (ca *Compressed_Array) Append(value uint64) {
 	// TODO: This is just an example since we don't always want to get the last checkpoint.
 	// E.g. If the diff is too large, we want to search all checkpoint and check if one exists that fits
 	//        before adding a new checkpoint.
-	if len(ca.D) == 0 {
+	if len(*ca.D) == 0 {
 		ca.create_checkpoint(value)
 		return
 	}
 
 	// Get some values...
-	checkpoint_idx := ca.get_previous_checkpoint_idx(uint64(len(ca.D) - 1))
+	checkpoint_idx := ca.get_previous_checkpoint_idx(uint64(len(*ca.D) - 1))
 	checkpoint_value := ca.get_checkpoint_value(checkpoint_idx)
 
 	// Again, example implementation for now: if we cannot fit the value, create a new checkpoint...
 	if ca.needs_new_checkpoint(checkpoint_idx, value) {
-		ca.create_checkpoint(value)
+		matching_checkpoint, did_find_matching_checkpoint := ca.search_for_matching_checkpoint(value)
+		if did_find_matching_checkpoint {
+			ca.handle_restructuring(matching_checkpoint, value)
+		} else {
+			ca.create_checkpoint(value)
+		}
 		return
 	}
 
@@ -266,29 +229,37 @@ func (ca *Compressed_Array) Append(value uint64) {
 	}
 	diff := uint64(_diff)
 	if diff == 0 {
-		ca.create_checkpoint(value)
+		matching_checkpoint, did_find_matching_checkpoint := ca.search_for_matching_checkpoint(value)
+		if did_find_matching_checkpoint {
+			ca.handle_restructuring(matching_checkpoint, value)
+		} else {
+			ca.create_checkpoint(value)
+		}
 		return
 	}
 
 	// Otherwise we need to add the value to a normal data entry by means of checking for a free bit string.
 
-	for i := checkpoint_idx + 1; i < uint64(len(ca.D)); i++ {
+	for i := checkpoint_idx + 1; i < uint64(len(*ca.D)); i++ {
 		if ca.is_checkpoint(i) {
 			break // Stop searching since we have reached the next checkpoint.
 		}
 		for j := uint64(0); j < c__MAX_NUM_VALUES_PER_ENTRY; j++ {
-			if ca.D[i]&(c__BITMASK__OUR_BITS<<(j*c__NUM_BITS_PER_VALUE)) != 0 {
+			if (*ca.D)[i]&(c__BITMASK__OUR_BITS<<(j*c__NUM_BITS_PER_VALUE)) != 0 {
 				continue
 			}
 			diff, does_need_new_checkpoint, is_negative := ca.calculate_difference(checkpoint_value, value)
 			if !does_need_new_checkpoint {
-				appropriate_multiplier, new_diff := ca.find_appropriate_multiplier(diff)
-				ca.D[i] |= uint64(new_diff) << (j * c__NUM_BITS_PER_VALUE)
+				(*ca.D)[i] |= uint64(diff) << (j * c__NUM_BITS_PER_VALUE)
 				ca.apply_sign_infos(i, j, is_negative)
-				ca.set_multiplier(i, j, appropriate_multiplier)
 			} else {
 				// We need to create a new checkpoint.
-				ca.create_checkpoint(value)
+				matching_checkpoint, did_find_matching_checkpoint := ca.search_for_matching_checkpoint(value)
+				if did_find_matching_checkpoint {
+					ca.handle_restructuring(matching_checkpoint, value)
+				} else {
+					ca.create_checkpoint(value)
+				}
 			}
 			return
 		}
@@ -296,13 +267,16 @@ func (ca *Compressed_Array) Append(value uint64) {
 
 	diff, does_need_new_checkpoint, is_negative := ca.calculate_difference(checkpoint_value, value)
 	if !does_need_new_checkpoint {
-		appropriate_multiplier, new_diff := ca.find_appropriate_multiplier(diff)
-		ca.D = append(ca.D, uint64(new_diff))
-		ca.apply_sign_infos(uint64(len(ca.D)-1), 0, is_negative)
-		ca.set_multiplier(uint64(len(ca.D)-1), 0, appropriate_multiplier)
+		(*ca.D) = append((*ca.D), uint64(diff))
+		ca.apply_sign_infos(uint64(len(*ca.D)-1), 0, is_negative)
 	} else {
 		// We need to create a new checkpoint.
-		ca.create_checkpoint(value)
+		matching_checkpoint, did_find_matching_checkpoint := ca.search_for_matching_checkpoint(value)
+		if did_find_matching_checkpoint {
+			ca.handle_restructuring(matching_checkpoint, value)
+		} else {
+			ca.create_checkpoint(value)
+		}
 	}
 }
 
@@ -312,7 +286,7 @@ func (ca *Compressed_Array) Get(idx uint64) uint64 {
 		current_logical_index    uint64
 	)
 
-	for i := uint64(0); i < uint64(len(ca.D)); i++ {
+	for i := uint64(0); i < uint64(len(*ca.D)); i++ {
 		if ca.is_checkpoint(i) {
 			current_checkpoint_value = ca.get_checkpoint_value(i)
 			if current_logical_index == idx {
@@ -321,17 +295,16 @@ func (ca *Compressed_Array) Get(idx uint64) uint64 {
 			current_logical_index++
 		} else {
 			for j := uint64(0); j < c__MAX_NUM_VALUES_PER_ENTRY; j++ {
-				v := (ca.D[i] >> (j * c__NUM_BITS_PER_VALUE)) & c__BITMASK__OUR_BITS
+				v := ((*ca.D)[i] >> (j * c__NUM_BITS_PER_VALUE)) & c__BITMASK__OUR_BITS
 				if v == 0 {
 					continue
 				}
 				if current_logical_index == idx {
-					multiplier := ca.get_multiplier(i, j)
 					is_negative := ca.get_is_negative(i, j)
 					if is_negative {
-						return current_checkpoint_value - (v * multiplier)
+						return current_checkpoint_value - v
 					} else {
-						return current_checkpoint_value + (v * multiplier)
+						return current_checkpoint_value + v
 					}
 				}
 				current_logical_index++
